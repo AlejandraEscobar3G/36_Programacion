30e7c0e41a79aea0f29b1fac105c7e97dd9e4875
AlejandraEscobar3G <ale.fesico@gmail.com>
2016-10-06 10:04:43 -0500

    Ejercicio 2 Funciones del capitan terminado

--- Funciones/Ejercicio2 - Par o impar/main.js
+++ Funciones/Ejercicio2 - Par o impar/main.js
@@ -1,4 +1,15 @@
 // Escribe aquí tu codigo Javascript
+function calcularPar_Impar(a){
+	if (a%2 === 0){
+		return "es par";
+	} else{
+		return "es impar";
+	}
+}
 
+//Solicitando numeros
+var a = Number(prompt("Dame un numero entero"));
+var resultado = calcularPar_Impar(a);
+document.write("El numero " + a + " " + resultado);
 
 
