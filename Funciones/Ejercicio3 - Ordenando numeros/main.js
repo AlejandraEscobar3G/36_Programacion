de9386c52697f187a8fa274dac4a0aef09ea54c2
AlejandraEscobar3G <ale.fesico@gmail.com>
2016-10-07 15:56:04 -0500

    Ejercicio 3 con sublime terminado

--- Funciones/Ejercicio3 - Ordenando numeros/main.js
+++ Funciones/Ejercicio3 - Ordenando numeros/main.js
@@ -1,4 +1,25 @@
-// Escribe aquí tu codigo Javascript
+/*var numeros = [];
+ 
+numeros.push(prompt("Introduce el primer número: "));
+numeros.push(prompt("Introduce el segundo número: "));
+numeros.push(prompt("Introduce el tercer número: "));
+ 
+var ordenados = numeros.sort(function (a,b) {
+    return a - b;
+});
+ 
+alert(ordenados);
+*/
+var numeros=[];
+function ordenarNumeros(numeros){
+	var orden = numeros.sort(function(a,b){
+		return a - b;
+	});
+	return orden;
+}
 
-
-
+numeros.push(prompt("Introduce el primer numero"));
+numeros.push(prompt("Introduce el segundo numero"));
+numeros.push(prompt("Introduce el tercer numero"));
+var ordenados = ordenarNumeros(numeros);
+document.write(ordenados);
